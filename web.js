1259dca1ca0445c5d432c8b7af52755b363e585f
viral <viraljogani@gmail.com>
2014-03-15 20:11:22 -0400

    update web.js

--- web.js
+++ web.js
@@ -6,4 +6,6 @@ app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
 }).configure(function() {
     app.use('/scripts', express.static(__dirname + '/scripts'));
+    app.use('/images', express.static(__dirname + '/images'));
+    app.use('/css', express.static(__dirname + '/css'));
 }).listen(port);
