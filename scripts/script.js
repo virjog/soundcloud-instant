ab7d1a017d40f0415375a6e015e4f8b3820250b2
viral <viraljogani@gmail.com>
2014-03-16 21:59:27 -0400

    update script

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,8 @@
-
 $('#search').keyup(function() {
-  $.ajax({
+    $.ajax({
       url: "https://api.soundcloud.com/tracks.json?" + $('#search').serialize(),
       dataType: 'json',
-      beforeSend: 
+      beforeSend:
         function(data){
           $('#sounds').empty();
         },
@@ -13,7 +12,7 @@ $('#search').keyup(function() {
           var items = [];
           $.each(data, function(key, val){
             items.push("<div id='tracks_list'><a data-artist='"+val.user.username+"' data-title='"+val.title+ "' data-url=" + val.stream_url + " href='javascript:void();'><li><h2>"+val.title+"</h2>\
-            <span class='plays'>" + val.playback_count+ " plays  -  <b>"+ val.user.username+  "</b></span></li></a>");
+            <span class='plays'>" + val.user.username +   " -  <b>" + msToTime(val.duration)  +  "</b></span></li></a>");
           });
           $('#sounds').html(items.join(' '));
           trackClick();
@@ -28,6 +27,21 @@ window.onload = function() {
 
 var clientid = 'client_id=2010872379d388118fe90f01ace38d03';
 
+function msToTime(duration) {
+          var milliseconds = parseInt((duration%1000)/100)
+              , seconds = parseInt((duration/1000)%60)
+              , minutes = parseInt((duration/(1000*60))%60)
+              , hours = parseInt((duration/(1000*60*60))%24);
+
+          hours = (hours < 10) ? "0" + hours : hours;
+          minutes = (minutes < 10) ? "0" + minutes : minutes;
+          seconds = (seconds < 10) ? "0" + seconds : seconds;
+          if(hours == "00")
+            return minutes + ":" + seconds;
+          else
+            return hours + ":" + minutes + ":" + seconds;
+        }
+
 function trackClick(){
   $('#tracks_list a').click(function(){
     var url= $(this).data('url') +"?"+ clientid;
@@ -39,8 +53,7 @@ function trackClick(){
   audioPlayer.src = url;
   audioPlayer.load();
   document.getElementById('player').play();
-  document.title="Playing - Soundcloud Instant"
+  document.title="Playing - SoundCloud Instant"
     return false;
-    
   });
 }
